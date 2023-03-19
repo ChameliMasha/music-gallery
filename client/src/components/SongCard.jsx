fa80551e667f477c661568246f4db56a9944cbba
ChameliMasha <mashapitumpe@gmail.com>
2023-03-19 19:14:08 +0530

    changes2

--- client/src/components/SongCard.jsx
+++ client/src/components/SongCard.jsx
@@ -1,11 +1,46 @@
 import React from 'react'
 import { motion } from "framer-motion"
 import { IoTrash } from 'react-icons/io5'
+import {
+  deleteSongById,
+  getAllAlbums,
+  getAllArtist,
+  getAllSongs,
+} from "../api"
+import { actionType } from "../context/reducer"
+import { storage } from "../config/firebase.config"
+//import { deleteSongById } from '../api'
+import { useStateValue } from "../context/StateProvider";
 
-const SongCard = ({data, index}) => {
+
+// const deleteObject = (data) => {
+
+//   if (type === "song") {
+//     deleteSongById(data._id).then((res) => {
+//       if (res.data) {
+    
+//       }
+//     });
+//   }
+// };
+
+const SongCard = ({ data, index }) => {
+  const [{ allUsers, user, getAllArtist, allAlbums, song, isSongPlaying }, dispatch] = useStateValue();
+  
+  // const addToContext = () => {
+  //   if (!isSongPlaying) {
+  //     dispatch({
+  //       type: actionType.SET_ISSONG_PLAYING,
+  //       isSongPlaying: true
+  //     })
+  //   }
+  // }
   return (
     
-    <motion.div className='relatoive w-40 min-w-210 px-2 py-4 cursor-pointer hover:bg-card shadow-md rounded-lg flex flex-col items-center'>
+    <motion.div className='relatoive w-40 min-w-210 px-2 py-4 cursor-pointer hover:bg-card shadow-md rounded-lg flex flex-col items-center'
+    
+    // onClick={type==='song' && addToContext}
+    >
       <div className='w-4= min-w-[160px] h-40 min-h-[160px] rounded-lg drop-shadow-lg relative overflow-hidden'>
         <motion.img
           
@@ -14,13 +49,16 @@ const SongCard = ({data, index}) => {
           className='w-full h-full rounded-lg object-cover'
         />
       </div>
-      <p className='text-base text-headingColor font-semibold my-2'>
+      <p className='text-base text-center text-headingColor font-semibold my-2'>
         {data.name.length > 25 ? `${data.name.slice(0, 25)}..` : data.name}
+        {data.artist && (
+          
         <span className='block text-sm text-gray-400 my-1'>
           
           {data.artist.length > 25 ? `${data.artist.slice(0, 25)}..` : data.artist }
         
         </span>
+      )}
 
       </p>
       <div>
